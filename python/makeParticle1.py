c1190bf50bac0e4be864d6c591039fda056b8223
tamura_kazuma <kazt23@gmail.com>
2021-01-02 00:47:31 +0900

    3 terrain 追加

--- python/makeParticle1.py
+++ python/makeParticle1.py
@@ -21,34 +21,6 @@ def Auto_city():# Random Create
         get_coordinates(w, d)
     print(coordinates)
 
-def Create_field():
-    cmds.polyPlane(h=200, w=200, cuv=2, sy=100, sx=100, ch=1, ax=(0, 1, 0))
-    # 平らな表面を作成 
-    sl_mesh = cmds.ls(sl=True) # オブジェクトの存在確認
-    sl_plane = cmds.rename(sl_mesh, 'terrain_mdl') # オブジェクトが新しい名前に変更
-
-    # マテリアル作成
-    cmds.shadingNode('lambert', asShader=1)
-    
-    sl_mat = cmds.ls(sl=True)
-    cmds.rename(sl_mat, 'terrain')
-    cmds.sets(renderable=True, empty=1, noSurfaceShader=True, name='terrainSG')
-    cmds.connectAttr('terrain.outColor', 'terrainSG.surfaceShader', f=1)
-
-    # マテリアルアサイン
-    cmds.select(sl_plane)
-    cmds.sets(forceElement='terrainSG', e=1)
-
-    # デフォームテクスチャ作成
-    cmds.textureDeformer(vectorStrength=(1, 1, 1), exclusive="", direction="Handle", strength=10, envelope=1, vectorOffset=(0, 0, 0), vectorSpace="Object", offset=0, pointSpace="UV")
-    cmds.select('textureDeformerHandle1', r=1)
-
-    # テクスチャのかわりにとりあえずノイズをアサイン
-    cmds.shadingNode('noise', asTexture=1)
-    cmds.connectAttr('noise1.outColor', 'textureDeformer1.texture', force=1)
-    
-
-
 def get_coordinates(w, d):
     name = cmds.ls(sl = True)
     x = cmds.getAttr(name[0] + ".tx")
--- python/my_terrain.py
+++ python/my_terrain.py
@@ -2,27 +2,41 @@
 import random
 import maya.cmds as cmds
 
-def Create_terrain():
-    poly = cmds.polyPlane(h=100.0, w=100.0, cuv=2, sy=50, sx=50, ch=1, ax=(0, 1, 0))
+def Create_random_terrain(): # ランダム
+    poly = cmds.polyPlane(h=200.0, w=200.0, cuv=2, sy=100, sx=100, ch=1, ax=(0, 1, 0))
     ver = cmds.polyListComponentConversion(poly[0], toVertex=True)
     verE = cmds.filterExpand(ver, selectionMask=31)
     for obj in verE:
-        x = random.uniform(-0.1, 0.1)
-        y = random.uniform(0.0, 2.0)
-        z = random.uniform(-0.1, 0.1)
+        x = random.uniform(-2.0, 2.0)
+        y = random.uniform(0.0, 3.0)
+        z = random.uniform(-2.0, 2.0)
         cmds.select(obj, r=True)
         cmds.move(x, y, z, r=True)
 
-def Create_mountain(): # 未完成
-    poly = cmds.polyPyramid(w=10.0, cuv=2, ch=1, ax=(0, 1, 0))
+def Create_noise_terrain(): # ノイズ
+    cmds.polyPlane(h=200, w=200, cuv=2, sy=100, sx=100, ch=1, ax=(0, 1, 0))
+    #cmds.textureDeformer(vectorStrength=(1, 1, 1), exclusive="", direction="Handle", strength=10, envelope=1, vectorOffset=(0, 0, 0), vectorSpace="Object", offset=0, pointSpace="UV")
+    cmds.textureDeformer(vectorStrength=(1, 1, 1), exclusive="", direction="Handle", strength=random.uniform(1, 20), envelope=1, vectorOffset=(0, 0, 0), vectorSpace="Object", offset=0, pointSpace="UV")
+    cmds.select('textureDeformerHandle1', r=1)
+    cmds.shadingNode('noise', asTexture=1)
+    cmds.connectAttr('noise1.outColor', 'textureDeformer1.texture', force=1)
+
+def Create_alpha_terrain(): # ランダム + ノイズ
+    poly = cmds.polyPlane(h=200, w=200, cuv=2, sy=100, sx=100, ch=1, ax=(0, 1, 0))
+    #cmds.textureDeformer(vectorStrength=(1, 1, 1), exclusive="", direction="Handle", strength=10, envelope=1, vectorOffset=(0, 0, 0), vectorSpace="Object", offset=0, pointSpace="UV")
+    cmds.textureDeformer(vectorStrength=(1, 1, 1), exclusive="", direction="Handle", strength=random.uniform(1, 20), envelope=1, vectorOffset=(0, 0, 0), vectorSpace="Object", offset=0, pointSpace="UV")
+    cmds.select('textureDeformerHandle1', r=1)
+    cmds.shadingNode('noise', asTexture=1)
+    cmds.connectAttr('noise1.outColor', 'textureDeformer1.texture', force=1)
     ver = cmds.polyListComponentConversion(poly[0], toVertex=True)
     verE = cmds.filterExpand(ver, selectionMask=31)
     for obj in verE:
-        x = random.uniform(-0.1, 0.1)
-        y = random.uniform(-1.0, 1.0)
-        z = random.uniform(-0.1, 0.1)
+        x = random.uniform(-1.0, 1.0)
+        y = random.uniform(1.0, 3.0)
+        z = random.uniform(-1.0, 1.0)
         cmds.select(obj, r=True)
         cmds.move(x, y, z, r=True)
 
-#Create_terrain()
-Create_mountain()
+# https://note.com/tara_panda/n/n09fef30a06e9
+
+Create_alpha_terrain()
